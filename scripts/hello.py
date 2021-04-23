c9dd7109444184e6a531a6e441aae64d27319340
Zhou FENG <42152221+zfengg@users.noreply.github.com>
2021-04-23 09:10:07 +0800

    Create hello.py

---
+++ scripts/hello.py
@@ -0,0 +1,13 @@
+# setup environment
+import numpy as np
+import pandas as pd 
+import scipy 
+import matplotlib.pyplot as plt
+
+# Test of em
+a = "a"
+b = 1113
+c = 2323.334
+
+print("Hello world!")
+
